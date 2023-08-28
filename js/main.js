e9a5ffd9ad6e9f4a728b939206354481250eee04
davronme <davronergashev232@gmail.com>
2023-08-28 16:12:16 +0500

    add all code ater lightbox open and close sorry

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+const modifires = {
+  imgSHowcaseActiveBotton: 'img-showcase__active-thumbnail-button',
+  shoppingCardModalOpen: 'site-header__cart-modal--open',
+  lightboxOpen: 'lightbox__open'
+}
+
+
+// SHOPPING CARD
 const elSiteHeaderCardLink = document.querySelector('.js-site-header__cart-link');
 const elShoppingCardModal = document.querySelector('.site-header__cart-modal');
 
@@ -6,8 +14,53 @@ if (elSiteHeaderCardLink) {
   elSiteHeaderCardLink.addEventListener('click', function(evt){
     evt.preventDefault();
 
-    elShoppingCardModal.classList.toggle('site-header__cart-modal--open')
+    elShoppingCardModal.classList.toggle(modifires.shoppingCardModalOpen);
 
+  });
+};
+
+// SHOWCASE THUMBNAILS
+
+const elsImgShowcaseThumbnailButton = document.querySelectorAll('.js-img-showcase__thumbnail-button');
+const elImgShowcaseActiveImg = document.querySelector('.img-showcase__active-img')
+
+elsImgShowcaseThumbnailButton.forEach( function (elButton){
+  elButton.addEventListener('click', function(){
+
+    // REMOVE ACTIVE CLASS FROM ALL ELEMENTS
+    elsImgShowcaseThumbnailButton.forEach( function (elButton){
+      elButton.classList.remove(modifires.imgSHowcaseActiveBotton);
+    });
+    // ADD ACTIVE CLASS FOR CLICKED ELEMENT
+    elButton.classList.add(modifires.imgSHowcaseActiveBotton);
+
+    // UPDATE ACTIVE IMG SRC ACCORDINGLY
+    elImgShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig
+    elImgShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetena} 2x`;
+
+    console.log(elButton.dataset.imgShowcaseRetena);
   })
+});
 
+
+
+// LIGHTBOX
+
+const elImgShowcaseLightboxToggle = document.querySelector('.js-img-showcase__lightbox-toggle');
+const elLightbox = document.querySelector('.lightbox')
+const elLightboxClose = document.querySelector('.js-lightbox__close')
+
+
+
+
+if (elImgShowcaseLightboxToggle) {
+  elImgShowcaseLightboxToggle.addEventListener('click', function () {
+    elLightbox.classList.add(modifires.lightboxOpen);
+  })
+}
+
+if (elLightboxClose) {
+  elLightboxClose.addEventListener('click', function () {
+    elLightbox.classList.remove(modifires.lightboxOpen);
+  })
 }
