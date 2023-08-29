71fb67a895a294b81df77d1eae52e3007be33d83
davronme <davronergashev232@gmail.com>
2023-08-29 20:39:21 +0500

    chack some mistekes from old code and add showcase control

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const modifires = {
   imgSHowcaseActiveBotton: 'img-showcase__active-thumbnail-button',
+  imgLightboxSHowcaseActive: 'img-showcase__active-thumbnail',
   shoppingCardModalOpen: 'site-header__cart-modal--open',
   lightboxOpen: 'lightbox__open'
 }
@@ -49,8 +50,6 @@ const elLightbox = document.querySelector('.lightbox')
 const elLightboxClose = document.querySelector('.js-lightbox__close')
 
 
-
-
 if (elImgShowcaseLightboxToggle) {
   elImgShowcaseLightboxToggle.addEventListener('click', function () {
     elLightbox.classList.add(modifires.lightboxOpen);
@@ -64,21 +63,94 @@ if (elLightboxClose) {
 }
 
 // LIGHTBOX SHOWCASE
-const elsImgLightboxThumbnailButton = document.querySelectorAll('.js-img-lightbox__thumbnail-button');
-const elImgLightboxShowcaseActiveImg = document.querySelector('.img-lightbox-showcase__active-img')
+const elsImgLightboxThumbnailButton = elLightbox.querySelectorAll('.js-img-lightbox__thumbnail-button');
+const elImgLightboxShowcaseActiveImg = elLightbox.querySelector('.img-lightbox-showcase__active-img')
+const elsLightboxImgThumbnail = elLightbox.querySelectorAll('.img-showcase__thumbnail');
+
 
 elsImgLightboxThumbnailButton.forEach( function (elButton){
   elButton.addEventListener('click', function(){
 
     // REMOVE ACTIVE CLASS FROM ALL ELEMENTS
-    elsImgLightboxThumbnailButton.forEach( function (elButton){
-      elButton.classList.remove(modifires.imgSHowcaseActiveBotton);
+    elsLightboxImgThumbnail.forEach( function (elLightboxImgThumbnail){
+      elLightboxImgThumbnail.classList.remove(modifires.imgLightboxSHowcaseActive);
     });
     // ADD ACTIVE CLASS FOR CLICKED ELEMENT
-    elButton.classList.add(modifires.imgSHowcaseActiveBotton);
+    elButton.parentElement.classList.add(modifires.imgLightboxSHowcaseActive);
 
     // UPDATE ACTIVE IMG SRC ACCORDINGLY
     elImgLightboxShowcaseActiveImg.src = elButton.dataset.imgShowcaseBig
     elImgLightboxShowcaseActiveImg.srcset = `${elButton.dataset.imgShowcaseBig} 1x, ${elButton.dataset.imgShowcaseRetena} 2x`;
   })
 });
+
+
+// CONTROL ELEMENTS
+
+const elImgShowcasePreviosImg = document.querySelector('.img-showcase__previos-img');
+const elImgShowcaseNextImg = document.querySelector('.img-showcase__next-img');
+
+// Img Showcase Next Img
+
+if (elImgShowcaseNextImg) {
+  elImgShowcaseNextImg.addEventListener('click', function(){
+    // find active element
+    const elActiveTtem = elLightbox.querySelector('.img-showcase__active-thumbnail');
+
+    // remove active class from element
+    elActiveTtem.classList.remove('img-showcase__active-thumbnail')
+
+    // console.log(elActiveTtem)
+    // add active class to Next item
+    // console.log(elsLightboxImgThumbnail[0]);
+    let elNextActiveItem;
+    if (elActiveTtem.nextElementSibling === null) {
+      elNextActiveItem = elsLightboxImgThumbnail[0]
+      // elsLightboxImgThumbnail[0].classList.add('img-showcase__active-thumbnail')
+    }
+    else{
+      elNextActiveItem = elActiveTtem.nextElementSibling
+      // elActiveTtem.nextElementSibling.classList.add('img-showcase__active-thumbnail');
+    }
+
+    elNextActiveItem.classList.add('img-showcase__active-thumbnail');
+
+    // UPDATE ACTIVE IMG SRC ACCORDINGLY
+    elImgLightboxShowcaseActiveImg.src = elNextActiveItem.children[0].dataset.imgShowcaseBig
+    elImgLightboxShowcaseActiveImg.srcset = `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetena} 2x`;
+
+    // // UPDATE ACTIVE IMG SRC ACCORDINGLY
+    // elImgLightboxShowcaseActiveImg.src = elNextActiveItem.querySelector('.js-img-lightbox__thumbnail-button').dataset.imgShowcaseBig
+    // elImgLightboxShowcaseActiveImg.srcset = `${elNextActiveItem.querySelector('.js-img-lightbox__thumbnail-button').dataset.imgShowcaseBig} 1x, ${elNextActiveItem.querySelector('.js-img-lightbox__thumbnail-button').dataset.imgShowcaseRetena} 2x`;
+  })
+}
+
+// Img Showcase Previos Img
+
+if (elImgShowcasePreviosImg) {
+  elImgShowcasePreviosImg.addEventListener('click', function(){
+    // find active element
+    const elActiveTtem = elLightbox.querySelector('.img-showcase__active-thumbnail');
+
+    // remove active class from element
+    elActiveTtem.classList.remove('img-showcase__active-thumbnail')
+
+    // add active class to Next item
+    let elNextActiveItem;
+    if (elActiveTtem.previousElementSibling === null) {
+      elNextActiveItem = elsLightboxImgThumbnail[3]
+      // elsLightboxImgThumbnail[0].classList.add('img-showcase__active-thumbnail')
+    }
+    else{
+      elNextActiveItem = elActiveTtem.previousElementSibling
+      // elActiveTtem.nextElementSibling.classList.add('img-showcase__active-thumbnail');
+    }
+
+    elNextActiveItem.classList.add('img-showcase__active-thumbnail');
+
+    // UPDATE ACTIVE IMG SRC ACCORDINGLY
+    elImgLightboxShowcaseActiveImg.src = elNextActiveItem.children[0].dataset.imgShowcaseBig
+    elImgLightboxShowcaseActiveImg.srcset = `${elNextActiveItem.children[0].dataset.imgShowcaseBig} 1x, ${elNextActiveItem.children[0].dataset.imgShowcaseRetena} 2x`;
+
+  })
+}
